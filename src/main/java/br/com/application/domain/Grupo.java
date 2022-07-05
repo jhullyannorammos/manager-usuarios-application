06857925b54213c58021c026458962725e8cd6c4
Juliano Ramos <jhullyannoors@gmail.com>
2022-07-04 22:24:23 -0300

    criando permissao.java e associando com grupos.java

--- src/main/java/br/com/application/domain/Grupo.java
+++ src/main/java/br/com/application/domain/Grupo.java
@@ -1,15 +1,52 @@
 package br.com.application.domain;
 
+import java.util.List;
+
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+
+@Entity
+@Table(name = "grupo")
 public class Grupo extends GenericDomain {
 
-	
+	@ManyToMany
+	@JoinTable(name = "grupo_permissao", joinColumns = @JoinColumn(name="grupo_id"),
+			inverseJoinColumns = @JoinColumn(name = "permissao_id"))
+	private List<Permissao> permissoes;
 	private String descricao;
 	private String nome;
 	
 	public Grupo() {
 		// TODO Auto-generated constructor stub
 	}
-	
+
+	public List<Permissao> getPermissoes() {
+		return permissoes;
+	}
+
+	public void setPermissoes(List<Permissao> permissoes) {
+		this.permissoes = permissoes;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
 	
 	
 }
---
+++ src/main/java/br/com/application/domain/Permissao.java
@@ -0,0 +1,40 @@
+package br.com.application.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "permissao")
+public class Permissao extends GenericDomain {
+	
+	@Column(nullable=false, length=40)
+	private String nome;
+	
+	@Column(nullable=false, length=80)
+	private String descricao;
+
+	public Permissao() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+}
