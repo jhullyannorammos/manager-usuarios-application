5574607d77f230dfc6dca5691f87a53753c50a98
Juliano Ramos <jhullyannoors@gmail.com>
2022-07-04 22:15:54 -0300

    criando grupo.java e relacionando com usuarios.java

---
+++ src/main/java/br/com/application/domain/Grupo.java
@@ -0,0 +1,15 @@
+package br.com.application.domain;
+
+public class Grupo extends GenericDomain {
+
+	
+	private String descricao;
+	private String nome;
+	
+	public Grupo() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	
+}
--- src/main/java/br/com/application/domain/Usuario.java
+++ src/main/java/br/com/application/domain/Usuario.java
@@ -1,10 +1,15 @@
 package br.com.application.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Transient;
 
@@ -13,6 +18,11 @@ import br.com.application.enumeracao.TipoUsuario;
 @SuppressWarnings("serial")
 @Entity
 public class Usuario extends GenericDomain {
+	
+	@ManyToMany
+	@JoinTable(name = "usuario_grupo", joinColumns = @JoinColumn(name="usuario_id"),
+			inverseJoinColumns = @JoinColumn(name = "grupo_id"))
+	private List<Grupo> grupos = new ArrayList<>();
 	
 	@Column(unique = true)
 	private String logon;
@@ -34,6 +44,14 @@ public class Usuario extends GenericDomain {
 	
 	public Usuario() {
 		// TODO Auto-generated constructor stub
+	}
+	
+	public List<Grupo> getGrupos() {
+		return grupos;
+	}
+	
+	public void setGrupos(List<Grupo> grupos) {
+		this.grupos = grupos;
 	}
 	
 	public String getLogon() {
